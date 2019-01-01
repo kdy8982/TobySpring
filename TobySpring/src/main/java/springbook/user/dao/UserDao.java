55d5dc4d5f45d65a7a8614277ac42ad4f5f4eceb
kdy <kdy8982@naver.com>
2019-01-01 22:41:13 +0900

    Refactoring
    getConnection() method

--- TobySpring/src/main/java/springbook/user/dao/UserDao.java
+++ TobySpring/src/main/java/springbook/user/dao/UserDao.java
@@ -8,32 +8,37 @@ import java.sql.SQLException;
 import springbook.user.domain.User;
 
 public class UserDao {
-	
+
 	Connection c;
 	PreparedStatement ps;
+
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		c = DriverManager.getConnection("jdbc:mysql://localhost:3306/spring", "root","1234");
+
+		c = getConnection();
 		ps = c.prepareStatement("insert into user values (?,?,?)");
 		ps.setString(1, user.getEmail());
 		ps.setString(2, user.getName());
 		ps.setString(3, user.getPassword());
 		ps.execute();
-		
+
 		ps.close();
 		c.close();
-		
+
 	}
-	
+
 	public void deleteAll() throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		c = DriverManager.getConnection("jdbc:mysql://localhost:3306/spring", "root","1234");
+		c = getConnection();
 		ps = c.prepareStatement("delete from user");
 		ps.execute();
 
 		ps.close();
 		c.close();
 	}
-	
-	
+
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		c = DriverManager.getConnection("jdbc:mysql://localhost:3306/spring", "root", "1234");
+		return c;
+	}
+
 }
