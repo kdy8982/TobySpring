a41d50a50d5364f4e2f572dce1fe762703ed21cd
kdy <kdy8982@naver.com>
2019-01-02 22:16:14 +0900

    Change DaoFactory.java to Spring applicationContext

--- TobySpring/src/main/java/springbook/user/dao/DaoFactory.java
+++ TobySpring/src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,14 +1,21 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DaoFactory {
 	
+	@Bean
 	public UserDao userDao() {
-		UserDao userDao = new UserDao();
-		userDao.connectionMaker = new DConnectionMaker();
-		
+		UserDao userDao = new UserDao(connectionMaker());
 		return userDao;
 	}
+	
+	@Bean
+	public ConnectionMaker connectionMaker() {
+		ConnectionMaker connectionMaker = new DConnectionMaker();
+		return connectionMaker;
+	}
+	
 }
--- TobySpring/src/main/java/springbook/user/dao/UserDaoTest.java
+++ TobySpring/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -2,6 +2,9 @@ package springbook.user.dao;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import springbook.user.domain.User;
 
 public class UserDaoTest {
@@ -10,7 +13,10 @@ public class UserDaoTest {
 		User user = new User("KimDaeYeon", "kdy8982@naver.com", "1234");
 		
 		// UserDao userDao = new UserDao(new DConnectionMaker()); // 클라이언트 단계에서 생성자로 어떠한 커넥션을 사용할지 주입해줌. 
-		UserDao userDao = new DaoFactory().userDao(); // DaoFactory클래스를 분리하여 , userDao(new DConnectionMaker)를 주입해준다.
+		// UserDao userDao = new DaoFactory().userDao(); // DaoFactory클래스를 분리하여 , userDao(new DConnectionMaker)를 주입해준다.
+		
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao userDao = applicationContext.getBean("userDao", UserDao.class);
 		
 		userDao.deleteAll();
 		userDao.add(user);
